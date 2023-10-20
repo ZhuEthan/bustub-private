a70141c562bedffe73fc8e8b9c1362aeccabb6c9
ZhuEthan <zhuyisong1994@gmail.com>
2023-10-20 08:14:28 -0700

    hash join

--- src/execution/hash_join_executor.cpp
+++ src/execution/hash_join_executor.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/hash_join_executor.h"
+#include "type/value_factory.h"
+
 
 namespace bustub {
 
@@ -22,10 +24,88 @@ HashJoinExecutor::HashJoinExecutor(ExecutorContext *exec_ctx, const HashJoinPlan
     // Note for 2023 Spring: You ONLY need to implement left join and inner join.
     throw bustub::NotImplementedException(fmt::format("join type {} not supported", plan->GetJoinType()));
   }
+  plan_ = plan;
+  left_child_ = std::move(left_child);
+  right_child_ = std::move(right_child);
 }
 
-void HashJoinExecutor::Init() { throw NotImplementedException("HashJoinExecutor is not implemented"); }
+void HashJoinExecutor::Init() {
+  left_child_->Init();
+  right_child_->Init();
+  ht_.clear();
 
-auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+  Tuple tuple;
+  RID rid;
+  //insert all right tuple to the hashtable;
+  while (right_child_->Next(&tuple, &rid)) {
+    JoinHashKey joinhashkey;
+    auto expressions = plan_->RightJoinKeyExpressions();
+    for (auto const &express : expressions) {
+      joinhashkey.joinkeys_.push_back(express->Evaluate(&tuple, right_child_->GetOutputSchema()));
+    }
+    ht_[joinhashkey].match_tuples_.push_back(tuple);
+  }
+}
+
+void HashJoinExecutor::GetOutputTuple(Tuple *tuple, bool is_matched) {
+  Tuple right_tuple;
+  if (is_matched) {
+    right_tuple = match_right_tuples_.back();
+    match_right_tuples_.pop_back();
+  }
+
+  std::vector<Value> values;
+  auto left_cols_cnt = plan_->GetLeftPlan()->OutputSchema().GetColumnCount();
+  auto right_cols_cnt = plan_->GetRightPlan()->OutputSchema().GetColumnCount();
+
+  for (uint32_t i = 0; i < left_cols_cnt; i++) {
+    values.push_back(left_tuple_.GetValue(&plan_->GetLeftPlan()->OutputSchema(), i));
+  }
+
+  if (is_matched) {
+    for (uint32_t i = 0; i < right_cols_cnt; i++) {
+      values.push_back(right_tuple.GetValue(&plan_->GetRightPlan()->OutputSchema(), i));
+    }
+  } else {
+    for (uint32_t i = 0; i < right_cols_cnt; i++) {
+      values.push_back(ValueFactory::GetNullValueByType(TypeId::INTEGER));
+    }
+  }
+
+  *tuple = Tuple(values, &GetOutputSchema());
+}
+
+
+auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  while (true) {
+    if (!match_right_tuples_.empty()) {
+      GetOutputTuple(tuple, true);
+      return true;
+    }
+
+    if (!left_child_->Next(&left_tuple_, rid)) {
+      return false;
+    }
+
+    //left join hashkey
+    JoinHashKey joinhashkey;
+    auto expressions = plan_->LeftJoinKeyExpressions();
+    for (auto const &express : expressions) {
+      joinhashkey.joinkeys_.push_back(express->Evaluate(&left_tuple_, left_child_->GetOutputSchema()));
+    }
+
+    if (ht_.count(joinhashkey) == 0 && plan_->GetJoinType() == JoinType::LEFT) {
+      GetOutputTuple(tuple, false);
+      return true;
+    }
+
+    if (ht_.count(joinhashkey) != 0) {
+      match_right_tuples_ = ht_[joinhashkey].match_tuples_;
+    }
+
+  }
+
+  return false;
+}
 
 }  // namespace bustub
